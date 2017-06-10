f4564a262eb17bfa9fdc6225dff3119a0c648b7b
vkarppinen <karppinenvaltteri@gmail.com>
2017-06-10 19:23:17 +0300

    started message-input component

--- src/messages.js
+++ src/messages.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MessageInput from './message-input.js'
+
 class Messages extends React.Component {
   render() {
     return (
       <div className="col-md-12">
         <div id="messages"></div>
+        <MessageInput />
       </div>
     );
   }
