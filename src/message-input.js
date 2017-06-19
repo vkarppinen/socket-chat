9ea6528f408973fa83dfb3d14ecb06b75d1a232b
vkarppinen <karppinenvaltteri@gmail.com>
2017-06-19 21:18:33 +0300

    handling of input changes

--- src/message-input.js
+++ src/message-input.js
@@ -2,15 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class MessageInput extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleClick() {
+    let data = {
+      // TODO: user: username,
+      message: this.state.input
+    };
+    if (data.message.length <= 0) return false;
+    // TODO: socket.emit('new-message', data);
+    console.log(data.message);
+    this.setState({
+      input: ''
+    });
+  }
+
   render() {
     return (
       <div id="message-input-area">
-        <form id="msg-sender" action="">
-          <fieldset>
-            <label>Send a message</label>
-            <input name="msg" id="msg" autocomplete="off" /><button>Send</button>
-          </fieldset>
-        </form>
+        <input value={this.state.input} onChange={(e) => this.handleChange(e)} placeholder="Type a message"/>
+        <button onClick={() => this.handleClick()}>Send</button>
       </div>
     );
   }
