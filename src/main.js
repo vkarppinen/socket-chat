01e83219994cdac7d51792416a6400f069d80691
vkarppinen <karppinenvaltteri@gmail.com>
2017-06-19 21:17:03 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -20,3 +20,49 @@ class Main extends React.Component {
 }
 
 export default Main;
+
+// TODO: Move all this functionality to components.
+// $(function () {
+
+//   let username = "";
+//
+//   
+//
+//   socket.on('username-exists', (msg) => {
+//     username = "";
+//     $('#alert').text(msg);
+//   });
+//
+//   socket.on('registration-success', (msg) => {
+//     console.log(msg);
+//     $('#users-list').show();
+//     $('#message-input-area').show();
+//     $('#register').hide();
+//   });
+//
+//   // render user message
+//   socket.on('new-message', (data) => {
+//     let $container = $("<div>", {"class": "message-container"});
+//     let $userinfo = $("<div>", {"class": "user"}).html(data.user);
+//     let $msg = $("<div>", {"class": "message"}).html(data.message);
+//     $userinfo.append($msg);
+//     $container.append($userinfo);
+//     $('#messages').append($container);
+//   });
+//
+//   socket.on('update-user-list', (data) => {
+//     $('#users-online').empty();
+//     for (let user in data) {
+//       if (data.hasOwnProperty(user)) {
+//         let nick = data[user].name;
+//         $('#users-online').append($('<span>').text(nick));
+//       }
+//     }
+//   })
+//
+//   socket.on('user-disconnect', (msg) => {
+//     // TODO: make android like toast.
+//     console.log(msg);
+//   });
+//
+// });
