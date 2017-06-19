d523b0d1f62227440bde1f23eeda68b9211967fb
vkarppinen <karppinenvaltteri@gmail.com>
2017-06-19 21:17:46 +0300

    refactoring

--- src/register-form.js
+++ src/register-form.js
@@ -5,15 +5,24 @@ class RegisterForm extends React.Component {
   render() {
     return (
       <div>
-        <form id="register">
-          <label for="nick">choose a nick to register to chat</label>
-          <input id="nick" name="nick" autocomplete="off" placeholder=""/>
-          <button>Send</button>
-          <div id="alert" class="text-danger"></div>
-        </form>
+        <label>choose a nick to register to chat</label>
+        <input id="nick" autoComplete="off" placeholder=""/>
+        <button>Send</button>
+        <div id="alert" className="text-danger"></div>
       </div>
     );
   }
 }
 
 export default RegisterForm;
+
+/** TODO: User registers to chat **/
+//   $('#register').submit(function (e) {
+//     e.preventDefault();
+//     let nick = $('#nick').val();
+//     username = nick;
+//     socket.emit('new-user', nick);
+//     $('#nick').val('');
+//     $('#alert').text('');
+//     return false;
+//   });
