cbdf8fb78cac7d172b175e6685eec48a79fef9c8
vkarppinen <karppinenvaltteri@gmail.com>
2017-06-22 08:06:30 +0300

    moved functionality to the new component

--- src/register-form.js
+++ src/register-form.js
@@ -2,12 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class RegisterForm extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleClick() {
+    let nick = this.state.input;
+    /* TODO: socket.emit('new-user', nick);
+       TODO: Here we need also the condition when the chosen nick is reserved
+       TODO: $('#alert').text('');
+    */
+    console.log("nick:", nick);
+    this.props.userNameHandler(nick);
+    this.setState({
+      input: ''
+    });
+  }
+
   render() {
     return (
       <div>
         <label>choose a nick to register to chat</label>
-        <input id="nick" autoComplete="off" placeholder=""/>
-        <button>Send</button>
+        <input value={this.state.input} onChange={(e) => this.handleChange(e)} id="nick" autoComplete="off" placeholder=""/>
+        <button onClick={() => this.handleClick()}>Send</button>
         <div id="alert" className="text-danger"></div>
       </div>
     );
@@ -25,4 +52,11 @@ export default RegisterForm;
 //     $('#nick').val('');
 //     $('#alert').text('');
 //     return false;
+//   });
+
+// socket.on('registration-success', (msg) => {
+//     console.log(msg);
+//     $('#users-list').show();
+//     $('#message-input-area').show();
+//     $('#register').hide();
 //   });
