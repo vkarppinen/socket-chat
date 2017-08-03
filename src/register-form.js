34088ea8335c8ef8e2ce3a3619bba58b4344f71c
vkarppinen <karppinenvaltteri@gmail.com>
2017-08-03 20:06:41 +0300

    add usernamehandling into form component

--- src/register-form.js
+++ src/register-form.js
@@ -18,11 +18,31 @@ class RegisterForm extends React.Component {
 
   handleClick() {
     let nick = this.state.input;
-    /* TODO: socket.emit('new-user', nick);
-       TODO: Here we need also the condition when the chosen nick is reserved
-       TODO: $('#alert').text('');
-    */
-    this.props.userNameHandler(nick);
+    this.props.socket.emit('new-user', nick);
+
+    this.props.socket.on('registration-success', (msg) => {
+      console.log(msg);
+      this.setState({
+        registrationError: '',
+        registrationSuccess: msg
+      });
+      document.getElementById("nick").style.display = 'none';
+      this.props.userNameHandler(nick)
+    });
+
+    this.props.socket.on('username-exists', (msg) => {
+      console.log(msg);
+      this.setState({
+        registrationError: msg
+      });
+    });
+
+    // TODO: Here we need also the condition when the chosen nick is reserved
+    //    TODO: $('#alert').text('');
+
+    // Only if the username is not reserved.
+    // this.props.username = nick;
+
     this.setState({
       input: ''
     });
@@ -31,31 +51,17 @@ class RegisterForm extends React.Component {
   render() {
     return (
       <div>
-        <label>choose a nick to register to chat</label>
-        <input value={this.state.input} onChange={(e) => this.handleChange(e)} id="nick" autoComplete="off" placeholder=""/>
-        <button onClick={() => this.handleClick()}>Send</button>
-        <div id="alert" className="text-danger"></div>
+        <div id="nick">
+          <label>choose a nick to register to chat</label>
+          <input value={this.state.input} onChange={(e) => this.handleChange(e)} 
+                 autoComplete="off" placeholder=""/>
+          <button onClick={() => this.handleClick()}>Send</button>
+        </div>
+        <div id="alert" className="text-danger">{this.state.registrationError}</div>
+        <div id="alert" className="text-info">{this.state.registrationSuccess}</div>
       </div>
     );
   }
 }
 
 export default RegisterForm;
-
-/** TODO: User registers to chat **/
-//   $('#register').submit(function (e) {
-//     e.preventDefault();
-//     let nick = $('#nick').val();
-//     username = nick;
-//     socket.emit('new-user', nick);
-//     $('#nick').val('');
-//     $('#alert').text('');
-//     return false;
-//   });
-
-// socket.on('registration-success', (msg) => {
-//     console.log(msg);
-//     $('#users-list').show();
-//     $('#message-input-area').show();
-//     $('#register').hide();
-//   });
