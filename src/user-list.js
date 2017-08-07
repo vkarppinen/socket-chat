ae382779aee2ed403ca74a804c24b32c8507bf77
vkarppinen <karppinenvaltteri@gmail.com>
2017-08-07 21:34:24 +0300

    render online users in component

--- src/user-list.js
+++ src/user-list.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class UserList extends React.Component {
+  
+  componentDidMount() {
+    this.props.socket.on('update-userlist', (users) => {
+      this.props.userlistHandler(users);
+    });
+  }
+  
+  renderUsers() {
+    const users = this.props.userlist;
+    if (users != undefined && users.length > 0) {
+      return (
+        <div>
+        {users.map(user => 
+            <div key={user.name} className="user">{user.name}</div>
+        )}
+        </div>
+      )
+    } else {
+      return
+    }
+    
+  }
+  
   render() {
     return (
       <div id="users-list">
         <div><strong>Users online:</strong></div>
-        <div id="users-online"></div>
+        {this.renderUsers()}
       </div>
     );
   }
